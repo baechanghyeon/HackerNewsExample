1dd8b579e0726624f17301b9ef01088bf2e19a2f
baechanghyeon <pek5302@naver.com>
2022-10-24 23:12:28 +0900

    코드 중복 제거 함수구현

--- app.js
+++ app.js
@@ -4,35 +4,40 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
+function getData(url) {
+  ajax.open("GET", url, false);
+  ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
 window.addEventListener("hashchange", function () {
-  console.log("해쉬체인지");
   const id = location.hash.substr(1);
 
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
   const title = document.createElement("h1");
 
   title.innerHTML = newsContent.title;
 
   content.appendChild(title);
 });
+
 for (let i = 0; i < 10; i++) {
+  const div = document.createElement("div");
   const li = document.createElement("li");
   const a = document.createElement("a");
 
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+    </li>
+  `;
 
-  li.appendChild(a);
-  ul.appendChild(li);
+  // ul.appendChild(div.children[0]);
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,27 +123,28 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement("div");
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+  ajax.open("GET", url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+}
+var newsFeed = getData(NEWS_URL);
 var ul = document.createElement("ul");
 window.addEventListener("hashchange", function () {
-  console.log("해쉬체인지");
   var id = location.hash.substr(1);
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-  var newsContent = JSON.parse(ajax.response);
+  var newsContent = getData(CONTENT_URL.replace("@id", id));
   var title = document.createElement("h1");
   title.innerHTML = newsContent.title;
   content.appendChild(title);
 });
 for (var i = 0; i < 10; i++) {
+  var div = document.createElement("div");
   var li = document.createElement("li");
   var a = document.createElement("a");
-  a.href = "#".concat(newsFeed[i].id);
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
-  li.appendChild(a);
-  ul.appendChild(li);
+  div.innerHTML = "\n    <li>\n      <a href=\"#".concat(newsFeed[i].id, "\">").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")</a>\n    </li>\n  ");
+
+  // ul.appendChild(div.children[0]);
+  ul.appendChild(div.firstElementChild);
 }
 container.appendChild(ul);
 container.appendChild(content);
@@ -172,7 +173,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55604" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61501" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
